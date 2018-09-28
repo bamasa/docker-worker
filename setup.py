fd69359d37cdf7771687ad1d34f9f31e566bc837
Leonid Gremyachikh <1123qqweaasdzzzxc@gmail.com>
2018-09-28 16:30:31 +0300

    fix docker version errors

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
     install_requires=[
         "lockfile",
         "requests>=2.5.1",
+        # TODO: error? docker.errors.InvalidVersion: API versions below 1.21
+        # are no longer supported by this library.
         "docker-py>=1.1.0",
         "six==1.9.0",
         "raven",
